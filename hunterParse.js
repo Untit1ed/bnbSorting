bc9527c7341569bf710cd7a8ab8d5b4a50872057
Untit1ed <untit1ed@mail.ru>
2020-11-03 17:36:17 -0800

    Update hunterParse.js

--- hunterParse.js
+++ hunterParse.js
@@ -1,5 +1,5 @@
 /* Airbnb Hunter - find top rated Airbnb apartments */
-const hunterURL = 'https://untit1ed.github.io/bnbSorting';
+const hunterURL = 'https://untit1ed.github.io/bnbSorting/';
 
 async function getJSON(url) {
 	return await fetch(url)
@@ -13,84 +13,82 @@ function extractSection(json) {
 	return sec.find((o) => o.section_component_type === 'LISTINGS_GRID');
 }
 
-var exportable = []
-var unique_ids = []
-var count_by_range = {}
-var items_per_grid = 50
+var exportable = [];
+var unique_ids = [];
+var count_by_range = {};
+var items_per_grid = 50;
 
-var dataState = JSON.parse(document.getElementById('data-state').innerHTML)
-var api_key = dataState.bootstrapData['layout-init'].api_config.key
+var dataState = JSON.parse(document.getElementById('data-state').innerHTML);
+var api_key = dataState.bootstrapData['layout-init'].api_config.key;
 
 
 async function parsePriceRange(c) {
-	console.log('Parcing from ' + c.min + ' to ' + c.max)
+	console.log('Parsing from ' + c.min + ' to ' + c.max);
 
 	c.anything_left = true
 	c.items_offset = 0
 
 	while (c.anything_left) {
-		var search_params = new URLSearchParams(location.href.split('?')[1])
-		search_params.set('items_per_grid', items_per_grid)
-		search_params.set('items_offset', c.items_offset)
-		search_params.set('key', api_key)
+		const searchParams = new URLSearchParams(location.href.split('?')[1]);
+		searchParams.set('items_per_grid', items_per_grid);
+		searchParams.set('items_offset', c.items_offset);
+		searchParams.set('key', api_key);
 
-		search_params.set('price_min', c.min)
-		search_params.set('price_max', c.max)
+		searchParams.set('price_min', c.min);
+		searchParams.set('price_max', c.max);
 
 		if (c.last_search_session_id)
-			search_params.set('last_search_session_id', c.last_search_session_id)
+			searchParams.set('last_search_session_id', c.last_search_session_id);
 
 		if (c.federated_search_session_id)
-			search_params.set('federated_search_session_id', c.federated_search_session_id)
+			searchParams.set('federated_search_session_id', c.federated_search_session_id);
 
 		hunterParse.innerHTML = `Parsing page ` + ((c.items_offset / items_per_grid) + 1) + ' range ' + c.min + '..' + c.max
 
-		//console.log('Request listings: ', search_params.toString())
-		var json = await getJSON(location.origin + '/api/v2/explore_tabs?' + search_params.toString())
+		//console.log('Request listings: ', searchParams.toString());
+		const json = await getJSON(`${location.origin}/api/v2/explore_tabs?${searchParams.toString()}`);
 		// 17 pages * 18 = 306 results limit
 		if (json.explore_tabs[0].home_tab_metadata.listings_count > 305) {
-			let half = Math.round((c.max - c.min) / 2)
+			const half = Math.round((c.max - c.min) / 2);
 
-			console.log('Bisect in half $' + half)
+			console.log('Bisect in half $' + half);
 
-			return await Promise.all([parsePriceRange(Object.assign({}, c, {
-				max: c.min + half
-			})),
-			parsePriceRange(Object.assign({}, c, {
-				min: c.min + half
-			}))
-			])
-		} else {
-			count_by_range[c.max] = json.explore_tabs[0].home_tab_metadata.listings_count
+			return await Promise.all([
+				parsePriceRange(Object.assign({}, c, { max: c.min + half })),
+				parsePriceRange(Object.assign({}, c, { min: c.min + half })),
+			]);
 		}
 
-		var section = extractSection(json)
-		if (section.listings.length > 0) {
-			for (let l of section.listings) {
-				if (unique_ids.indexOf(l.listing.id) == -1) {
-					l.listing.pricing_quote = l.pricing_quote
-					exportable.push(l.listing)
+		count_by_range[c.max] = json.explore_tabs[0].home_tab_metadata.listings_count;
 
-					unique_ids.push(l.listing.id)
+		var section = extractSection(json);
+		if (section.listings.length > 0) {
+			for (let listing of section.listings) {
+				if (unique_ids.indexOf(listing.listing.id) == -1) {
+					listing.listing.pricing_quote = listing.pricing_quote;
+					exportable.push(listing.listing);
+
+					unique_ids.push(listing.listing.id);
 				}
 			}
-		} else {
-			console.log('Empty listings')
+		}
+		else {
+			console.log('Empty listings');
 		}
 
 		// repeat
 		if (json.explore_tabs[0].pagination_metadata.has_next_page) {
-			c.items_offset += items_per_grid
+			c.items_offset += items_per_grid;
 
-			c.last_search_session_id = section.search_session_id
-			c.federated_search_session_id = json.metadata.federated_search_session_id
-		} else {
-
+			c.last_search_session_id = section.search_session_id;
+			c.federated_search_session_id = json.metadata.federated_search_session_id;
+		}
+		else {
 			// break loop
-			c.anything_left = false
+			c.anything_left = false;
 		}
 
-		hunterExport.innerHTML = `Export - ` + unique_ids.length
+		hunterExport.innerHTML = `Export - ` + unique_ids.length;
 	}
 }
 
@@ -126,13 +124,15 @@ async function initParse() {
 
 function exportListings() {
 	const remoteWindow = window.open(hunterURL);
-	window.addEventListener('message', function (e) {
+	window.addEventListener('message', (e) => {
 		if (e.data === 'ready') {
 			console.log('exporting data');
-			remoteWindow.postMessage(JSON.stringify({
+			const jsonString = JSON.stringify({
 				listings: exportable,
 				href: location.href.split('?')[1],
-			}), '*');
+			});
+			console.log(jsonString);
+			remoteWindow.postMessage(jsonString, '*');
 		}
 	});
 }
